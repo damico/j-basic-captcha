94c72c7178b8e61c18a7ea5aa7632d2c474653db
José Damico <jd.comment@gmail.com>
2019-07-24 20:02:45 -0300

    improved test unit

--- jbasiccaptcha/src/test/java/org/damico/jbasiccaptcha/GeneratorTest.java
+++ jbasiccaptcha/src/test/java/org/damico/jbasiccaptcha/GeneratorTest.java
@@ -14,75 +14,55 @@
  * language governing permissions and limitations under the License.
  *
  */
-
 package org.damico.jbasiccaptcha;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.UndeclaredThrowableException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Before;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class GeneratorTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public GeneratorTest( String testName )
-    {
-        super( testName );
-    }
+public class GeneratorTest{ 
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( GeneratorTest.class );
-    }
-
-    
-    public void testApp()
-    {
-    	
-    	byte[] seed = new byte[]{10,20,30,40,60,80,90,00,10,20,30,40,60,80,90,00};
+	private byte[] seed = new byte[]{10,20,30,40,60,80,90,00,10,20,30,40,60,80,90,00};
+	private Generator generatorA = null;
+	private Generator generatorB = null;
+	
+	@Before
+	public void setUp() {
+		
 		try {
-			Generator generator = new Generator(seed, new File("/tmp/captcha.png"));
-			
-			String otpPre = generator.getOtp();
-			
-			generator = new Generator(seed, generator.getOtpL());
-			
-			String otpPos = generator.getOtp();
-			
-			assertEquals(otpPre, otpPos);
-			
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UndeclaredThrowableException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			generatorA = new Generator(seed, new File("/tmp/captcha.png"));
+			generatorB = new Generator(seed, generatorA.getOtpL());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	
+	}
+	
+	
+    @Test
+    public void testGenerator(){
+			String otpPre = generatorA.getOtp();
+			String otpPos = generatorB.getOtp();
+			assertEquals(otpPre, otpPos);
+			
+    }
+    
+    @Test
+    public void testValidator() {
+    	try {
+			boolean isValid = generatorB.validateByWindow(1);
+			assertEquals(true, isValid);
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (UndeclaredThrowableException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
     }
 }
